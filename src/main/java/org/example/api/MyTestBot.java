83a55eabc5d3510780953f3739a6198ba7907661
Ilimzhin <79404011+Ilimzhin@users.noreply.github.com>
2022-02-13 16:42:02 +0600

    commit

--- src/main/java/org/example/api/MyTestBot.java
+++ src/main/java/org/example/api/MyTestBot.java
@@ -3,9 +3,14 @@ package org.example.api;
 import lombok.SneakyThrows;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.Message;
 import org.telegram.telegrambots.meta.api.objects.Update;
 import org.telegram.telegrambots.meta.api.objects.replykeyboard.InlineKeyboardMarkup;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.ReplyKeyboardMarkup;
 import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.InlineKeyboardButton;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.KeyboardButton;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.KeyboardRow;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -63,16 +68,120 @@ public class MyTestBot extends TelegramLongPollingBot {
 
 
         final var sendMessage = switch (update.getMessage().getText()) {
+//            Message message = update.getMessage();
             case "/start" -> buildStartSendMessage(chatId);
+            case "/start2" -> buildStart2SendMessage(chatId);
             case "/help" -> buildHelpSendMessage(chatId);
+            case "/help2" -> buildHelp2SendMessage(chatId);
             case "/language" -> buildChooseLanguageSendMessage(chatId);
             default -> throw new RuntimeException(UNSUPPORTED_COMMAND_ERROR_MESSAGE);
         };
         try {
             execute(sendMessage);
+//            setButtons(sendMsg);
         } catch (Exception e) {
             e.printStackTrace();
         }
+    }
+
+//    private void sendMsg(Message message, String text) {
+//        SendMessage sendMessage = new SendMessage();
+//        sendMessage.enableMarkdown(true);
+//        sendMessage.setChatId(message.getChatId().toString());
+//        sendMessage.setReplyToMessageId(message.getMessageId());
+//        sendMessage.setText(text);
+//    }
+//
+//    public void setButtons(SendMessage sendMessage) {
+//
+//        ReplyKeyboardMarkup replyKeyboardMarkup = new ReplyKeyboardMarkup();
+//        sendMessage.setReplyMarkup(replyKeyboardMarkup);
+//
+//        replyKeyboardMarkup.setSelective(true);
+//        replyKeyboardMarkup.setResizeKeyboard(true);
+//        replyKeyboardMarkup.setOneTimeKeyboard(false);
+//
+//        List<KeyboardRow> keyboardRowList = new ArrayList<>();
+//        KeyboardRow keyboardFirstRow = new KeyboardRow();
+//
+//        keyboardFirstRow.add(new KeyboardButton("/start"));
+//        keyboardFirstRow.add(new KeyboardButton("/language"));
+//
+//
+//        keyboardRowList.add(keyboardFirstRow);
+//        replyKeyboardMarkup.setKeyboard(keyboardRowList);
+//    }
+
+    private SendMessage buildHelp2SendMessage(String chatId) {
+        SendMessage sendMessage = new SendMessage();
+        Message message = new Message();
+
+        ReplyKeyboardMarkup replyKeyboardMarkup = new ReplyKeyboardMarkup();
+        sendMessage.setReplyMarkup(replyKeyboardMarkup);
+
+        replyKeyboardMarkup.setSelective(true);
+        replyKeyboardMarkup.setResizeKeyboard(true);
+        replyKeyboardMarkup.setOneTimeKeyboard(false);
+
+        List<KeyboardRow> keyboardRowList = new ArrayList<>();
+        KeyboardRow keyboardFirstRow = new KeyboardRow();
+
+        keyboardFirstRow.add(new KeyboardButton("/start"));
+        keyboardFirstRow.add(new KeyboardButton("/start2"));
+        keyboardFirstRow.add(new KeyboardButton("/language"));
+        keyboardFirstRow.add(new KeyboardButton("/help"));
+        keyboardFirstRow.add(new KeyboardButton("/help2"));
+
+
+        keyboardRowList.add(keyboardFirstRow);
+        replyKeyboardMarkup.setKeyboard(keyboardRowList);
+
+        sendMessage.enableMarkdown(true);
+        sendMessage.setChatId(chatId);
+        sendMessage.setReplyToMessageId(message.getMessageId());
+        sendMessage.setText("some text example");
+
+        return sendMessage;
+    }
+
+    private SendMessage buildStart2SendMessage(String chatId) {
+        SendMessage sendMessage = new SendMessage();
+        Message message = new Message();
+
+        ReplyKeyboardMarkup replyKeyboardMarkup = new ReplyKeyboardMarkup();
+        sendMessage.setReplyMarkup(replyKeyboardMarkup);
+
+//        replyKeyboardMarkup.setSelective(true);
+//        replyKeyboardMarkup.setResizeKeyboard(true);
+//        replyKeyboardMarkup.setOneTimeKeyboard(false);
+
+        List<KeyboardRow> keyboardRowList = new ArrayList<>();
+//        List<KeyboardRow> keyboardRowList = new ArrayList<>();
+
+        KeyboardRow keyboardFirstRow = new KeyboardRow();
+
+        keyboardFirstRow.add(new KeyboardButton("/start"));
+//        keyboardFirstRow.add((KeyboardButton) Arrays.asList(
+//                InlineKeyboardButton.builder().callbackData("123").text("123").build().getCallbackData(),
+//                InlineKeyboardButton.builder().callbackData("123").text("123").build().getCallbackData()));
+//                InlineKeyboardButton.builder().callbackData("/language").text("language").build()));
+//        keyboardFirstRow.add(String.valueOf(Arrays.asList(new KeyboardButton("/start"),
+//                new KeyboardButton("/start2"))));
+        keyboardFirstRow.add(new KeyboardButton("/start2"));
+        keyboardFirstRow.add(new KeyboardButton("/language"));
+        keyboardFirstRow.add(new KeyboardButton("/help"));
+        keyboardFirstRow.add(new KeyboardButton("/help2"));
+
+
+        keyboardRowList.add(keyboardFirstRow);
+        replyKeyboardMarkup.setKeyboard(keyboardRowList);
+
+        sendMessage.enableMarkdown(true);
+        sendMessage.setChatId(chatId);
+        sendMessage.setReplyToMessageId(message.getMessageId());
+        sendMessage.setText("Main menu: ");
+
+        return sendMessage;
     }
 
     private SendMessage buildStartSendMessage(String chatId) {
@@ -109,7 +218,9 @@ public class MyTestBot extends TelegramLongPollingBot {
         return sendMessage;
     }
 
+
     private SendMessage buildHelpSendMessage(String chatId) {
+
         var sendMessage = new SendMessage();
         var inlineKeyboardMarkup = new InlineKeyboardMarkup();
         var columnList = new ArrayList<List<InlineKeyboardButton>>();
